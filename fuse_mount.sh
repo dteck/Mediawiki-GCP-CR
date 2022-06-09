d32cc5e88aaea16282010d9dd0aac423cd548093
Mark Richards <darkteck04@gmail.com>
2022-06-09 15:54:42 -0400

    set bucket names as env vars

--- fuse_mount.sh
+++ fuse_mount.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #Mount the google bucket that has the configuration files - change "wiki-settings" to your bucket name
-gcsfuse -o allow_other --file-mode=777 --dir-mode=777 --implicit-dirs wiki-settings /var/www/html/LocalSettingsLink
+#gcsfuse -o allow_other --file-mode=777 --dir-mode=777 --implicit-dirs wiki-settings /var/www/html/LocalSettingsLink
+gcsfuse -o allow_other --file-mode=777 --dir-mode=777 --implicit-dirs ${configbucket} /var/www/html/LocalSettingsLink
 
 #Mount the google bucket that holds uploaded images/files - change "wiki-image-share" to your bucket name
-gcsfuse -o allow_other --file-mode=777 --dir-mode=777 --implicit-dirs wiki-image-share /var/www/html/imagesLink
+#gcsfuse -o allow_other --file-mode=777 --dir-mode=777 --implicit-dirs wiki-image-share /var/www/html/imagesLink
+gcsfuse -o allow_other --file-mode=777 --dir-mode=777 --implicit-dirs ${imagebucket} /var/www/html/imagesLink
 
 #start the apache server
 apache2-foreground
